55bc8ae5ecc33b5e68048d4524f7b02d0fd35acf
silver2v <s@leismann.com.br>
2022-06-04 23:32:57 -0300

    trimmed app.py

--- app.py
+++ app.py
@@ -1,65 +1,56 @@
-from distutils.command.upload import upload
 import os
-from string import ascii_uppercase
 from subprocess import call
-from turtle import width
-# from types import NoneType
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 from random import choice
 from string import ascii_letters, digits
-from cv2 import imread, IMREAD_UNCHANGED, resize, INTER_AREA, imwrite, imshow, waitKey, destroyAllWindows
+from cv2 import imread, resize, INTER_AREA, imwrite, imshow, waitKey, destroyAllWindows
+
 
 ROOT_UPLOAD_FOLDER = 'static/uploads'
 OUTPUT_FOLDER = 'output/final_output'
-ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg', 'gif', 'webp'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
-# original = ""
-# result = ""
-
-# Configure application
+# Initializing flask application
 app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER --> nesse momento é desnecessário
-# app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER --> nesse momento é desnecessário
 app.config['SECRET_KEY'] = 'some random string'
-# app.config['MAX_CONTENT_LENGTH'] = 0.01 * 1024 * 1024
 
-command = "python3 Bringing-Old-Photos-Back-to-Life/run.py"
-
-def allowed_file(filename):
+# Functions that checks if extension of the file is allowed
+#   the function returns a boolean value
+def allowed_extension(filename):
     return '.' in filename and \
         filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def id_generator(prefix='img', size=6, chars=ascii_letters + digits):
+# Function used later to help creating the 2nd part of the  name of the folder where both 
+#   original image (input) and its restored version (output)
+#   will be stored (1st part of the name of the folder, aka prefix is file name itself)
+#   This makes each folder name unique, and name of image can be preserved)
+#   Ex. folder name: my-image_kjdjksi.jpg
+def id_generator(prefix, size=6, chars=ascii_letters + digits):
     return prefix + '_' + ''.join(choice(chars) for _ in range(size))
 
 
 @app.route("/", methods=["GET", "POST"])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
+    if not request.method == 'POST':
+     return render_template('index.html')
+    
+    else:
+        # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
 
-        
-        
-        # if (file[-3:] or file[-4:]) not in ALLOWED_EXTENSIONS:
-        # if not file in ALLOWED_EXTENSIONS:
-        #     flash('Allowed file types only: %s' % (ALLOWED_EXTENSIONS))
-        #     return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
-        # empty file without a filename.
+        #   empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
+            flash('No selected file. Please choose an image and try again.')
             return redirect(request.url)
-        # print(file and allowed_file(file.filename)
-
-        if not (file and allowed_file(file.filename)):
+   
+        if not (file and allowed_extension(file.filename)):
             flash("Allowed file types: " + ", ".join(str(x) for x in ALLOWED_EXTENSIONS) +  "." + " Please try again.")
             
-            #  %s" % (str(x) for x in ALLOWED_EXTENSIONS))
         else:
             print('hey')
             filename = secure_filename(file.filename)
@@ -73,11 +64,12 @@ def upload_file():
             #     upload_folder = f'uploads/{new_folder}'
             
             os.makedirs(complete_upload_folder)
-            
-            app.config['UPLOAD_FOLDER'] = complete_upload_folder
-            output_folder = f'{complete_upload_folder}/output/final_output'
-            app.config['OUTPUT_FOLDER'] = output_folder
 
+
+            output_folder = f'{complete_upload_folder}/output/final_output'
+            #Obs.: not necessary to makedir output_folder because "run.py" will do it by itself
+        
+            # The CLI commands the original implementation uses
             command1 = f"python3 Bringing-Old-Photos-Back-to-Life/run.py --input_folder {complete_upload_folder} \
               --output_folder {complete_upload_folder}/output \
               --GPU -1"
@@ -88,7 +80,7 @@ def upload_file():
               --with_scratch"
             
 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(complete_upload_folder, filename))
 
             #### CREATED THIS STUFF TO DECREASE DIMENTIONS IF ABOVE 1M PIXELS TO SPEED UP PROCESSING
             img = imread(f'{complete_upload_folder}/{filename}')
@@ -97,17 +89,22 @@ def upload_file():
                 flash("""Seems like you uploaded a corrupted or not a true image file. 
                     Please try again with a new file.""")
                 return redirect(request.url)
-                # return redirect('/oops')
            
+            # Checking the image dimensions in pixels         
             print("height: " + str(img.shape[0]))
             print("width: ", img.shape[1])
 
-            width = img.shape[1]
             height = img.shape[0]
+            width = img.shape[1]
+            
             pixels = width * height
             print('total pixels:', pixels)
 
-            if pixels > 1_000_000:
+            reduce = True
+            max_pixels = 1_000_000
+            
+            # If dimenions of image bigger than  max_pixels, reduce 5% (while loop)
+            if reduce == True and pixels > max_pixels:
                 while pixels > 1_000_000:
                     width = int(width * 95 / 100)
                     height = int(height * 95 / 100)
@@ -122,18 +119,11 @@ def upload_file():
     
                 print('Resized Dimensions : ',resized.shape)
 
+            #### FINISHED THE ABOVE PART
 
-            #### FINISHED THAT PART
-
-            
-
-            # Just for AWS
+            # Checking if option "with scratch" is marked (here name as "extra")
             extra = request.form.get("extra")
-            # extra = ""
-            # if extra == "extra":
-            #     flash('The option "with scratch" is not available at the moment, please try again with it unchecked.')
-            #     return redirect(request.url)
-
+    
             if not extra == "extra":
                 call(command1, shell=True)
             else: 
@@ -143,8 +133,8 @@ def upload_file():
             ## getting the links to post the pre and post pics
             original = complete_upload_folder + "/" + filename
 
-            #sometimes the program changes the extension of the
-            #output file to png (eg if the original file was a jpg), 
+            #sometimes "restoration engine" (original implementation program) changes the extension of the
+            #output file to png (eg sometimes if the original file was a jpg), 
             # so that must be taken into consideration
             
             output_filename = None
@@ -156,32 +146,9 @@ def upload_file():
                 return redirect('/oops')
             
             result = output_folder + "/" + output_filename
-
-            
-
-            #return render_template("test.html", folder=complete_upload_folder, filename=filename)
-            # return render_template("test3.html", result=result)
-            
-            #return """<img src=f"{output_folder}/{filename}">"""
-
-            # return redirect(url_for('download_file', name=filename))
            
             return render_template('result.html', original=original, result=result )
-            # return redirect(url_for('voila'))
-
-    return render_template('index2.html')
-
-# not being used atm
-@app.route('/test')
-def show_test():
-    return render_template('test.html')
-
-
-# @app.route('/<name>')
-# def download_file(name):
-#     return send_from_directory(app.config[OUTPUT_FOLDER], name)
-
-
+          
 
 @app.route('/oops')
 def oops():
@@ -192,7 +159,15 @@ def about():
     return render_template("about.html")
     
     
-    
-
 if __name__=="__main__":
     app.run(host='0.0.0.0')
+
+
+### Bellow are parts of code from previous versions that are not being used anymore
+
+# app.config['UPLOAD_FOLDER'] = complete_upload_folder
+# app.config['OUTPUT_FOLDER'] = output_folder
+
+# @app.route('/<name>')
+# def download_file(name):
+#     return send_from_directory(app.config[OUTPUT_FOLDER], name)
